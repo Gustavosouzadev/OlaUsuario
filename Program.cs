e18cf1ce6b1191452f3ca2943bfb44a4ea6693b1
Gustavosouzadev <gustavoduti2003@gmail.com>
2021-03-29 00:29:19 -0300

    Finalizando OlaUsuario

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            Console.WriteLine("Escreva seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine($"Ola! {nome}. ");
+
+        }
+    }
+}
